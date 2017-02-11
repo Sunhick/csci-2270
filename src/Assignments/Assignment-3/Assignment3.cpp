405473fe987ddd6ebcdcfa2884479bb75c4d8578
Sunil <Sunhick@gmail.com>
2017-02-11 13:59:01 -0700

    Update Assignment3.cpp

--- src/Assignments/Assignment-3/Assignment3.cpp
+++ src/Assignments/Assignment-3/Assignment3.cpp
@@ -8,8 +8,6 @@ using namespace std;
 // main expects 1 argument, filename
 int main(int argc, char* argv[])
 {
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    start = std::chrono::system_clock::now();
     char* filename = argv[1];
     int n = atoi(argv[2]);
 	
@@ -34,13 +32,6 @@ int main(int argc, char* argv[])
         // free the final array
 
     }
-    
-    end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end-start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    
-    std::cout << "finished computation at " << std::ctime(&end_time)
-              << "elapsed time: " << elapsed_seconds.count() << "s\n";
     return 0;
 }
 
