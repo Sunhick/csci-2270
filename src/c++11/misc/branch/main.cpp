16dd866ba7cd561e8ea37b5886f8c5e78a035909
Sunil <Sunhick@gmail.com>
2017-02-26 00:36:46 -0700

    branch on variable assignment

--- src/c++11/misc/branch/main.cpp
+++ src/c++11/misc/branch/main.cpp
@@ -1,1 +1,61 @@
+//
+//  main.cpp
+//  new
+//
+//  Created by Sunil on 2/25/17.
+//  Copyright © 2017 Sunil. All rights reserved.
+//
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+// placement new
+// cite: http://madebyevan.com/obscure-cpp-features/
+#include "Process.hpp"
+
+using namespace std;
+
+void KillProcess(Process* p) {
+//    auto or auto* works! Due to type deduction rules
+//    auto* sys = dynamic_cast<SystemProcess*>(p);
+//    if (sys) {
+//        cout << "invoke system process kill" << endl;
+//        sys->kill();
+//    }
+//    
+//    auto* user = dynamic_cast<UserProcess*>(p);
+//    if(user) {
+//        cout << "invoke user process kill" << endl;
+//        user->kill();
+//    }
+
+//    Branch on variable declaration
+//    The above calls can be more compactly written as,
+
+    if(auto* sys = dynamic_cast<SystemProcess*>(p)) {
+        cout << "invoke system process kill" << endl;
+        sys->kill();
+    }
+    
+    if(auto* user = dynamic_cast<UserProcess*>(p)) {
+        cout << "invoke user process kill" << endl;
+        user->kill();
+    }
+}
+
+int main(int argc, const char * argv[]) {
+
+    vector<shared_ptr<Process>> processes =
+    {
+        shared_ptr<Process>(new SystemProcess(1212)),
+        shared_ptr<Process>(new UserProcess(3434))
+    };
+    
+    for(auto& p : processes) {
+        KillProcess(p.get());
+    }
+    
+    return 0;
+}
