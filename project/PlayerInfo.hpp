45daf66e6230386b21c414c5da17da2ed00457ec
Sunil <sunhick@gmail.com>
2017-04-28 13:07:35 -0600

    added utilities helper

---
+++ project/PlayerInfo.hpp
@@ -0,0 +1,30 @@
+//
+//  PlayerInfo.cpp
+//  HashTables
+//
+//  Created by Sunil on 4/28/17.
+//  Copyright © 2017 Sunil. All rights reserved.
+//
+
+#include <string>
+
+using namespace std;
+
+struct PlayerInfo {
+    int yearId;
+    
+    char bats;
+    char throws;
+    
+    float salary;
+    float weight;
+    float height;
+    
+    string teamId;
+    string playerId;
+    string leagueId;
+    string firstName;
+    string lastName;
+    string birthYear;
+    string birthCountry;
+};
--- project/main.cpp
+++ project/main.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 
 #include "HashTable.hpp"
+#include "Utilities.hpp"
 
 using namespace std;
 
@@ -35,48 +36,8 @@ void PopulateHashTable(string filename, HashTable* map) {
     std::getline(file, ignoreHeader);
     
     while(std::getline(file, line)) {
-        stringstream lineStream(line);
-        
-        // Format of the line:
-        // yearID : int
-        // teamID : int
-        // leagueID : int
-        // playerID : int
-        // salary : float
-        // firstName : string
-        // lastName : string
-        // birthYear : string
-        // birthCountry : string
-        // weight : float
-        // height : float
-        // bats : int
-        // throws : int
-        int yearId;
-        char bats, throws;
-        float salary, weight, height;
-        string teamId, playerId, leagueId, firstName, lastName, birthYear, birthCountry;
-        char ignoreComma;
-        
-        lineStream >> yearId;       lineStream >> ignoreComma;
-        // lineStream >> teamId;       lineStream >> ignoreComma;
-        std::getline(lineStream, teamId, ',');
-        // lineStream >> leagueId;     lineStream >> ignoreComma;
-        std::getline(lineStream, leagueId, ',');
-        // lineStream >> playerId;     lineStream >> ignoreComma;
-        std::getline(lineStream, playerId, ',');
-        lineStream >> salary;       lineStream >> ignoreComma;
-        // lineStream >> firstName;    lineStream >> ignoreComma;
-        std::getline(lineStream, firstName, ',');
-        // lineStream >> lastName;     lineStream >> ignoreComma;
-        std::getline(lineStream, lastName, ',');
-        // lineStream >> birthYear;    lineStream >> ignoreComma;
-        std::getline(lineStream, birthYear, ',');
-        // lineStream >> birthCountry; lineStream >> ignoreComma;
-        std::getline(lineStream, birthCountry, ',');
-        lineStream >> weight;       lineStream >> ignoreComma;
-        lineStream >> height;       lineStream >> ignoreComma;
-        lineStream >> bats;         lineStream >> ignoreComma;
-        lineStream >> throws;
+        auto playerInfo = Utilities::ConstructFrom(line);
+        cout << "haha" << endl;
     }
 }
 
