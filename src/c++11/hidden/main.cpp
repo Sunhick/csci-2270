824b6e7cce430ada9119599d559b9de523ad18c4
Sunil <sunhick@gmail.com>
2017-05-03 22:43:52 -0600

    some weird code

--- src/c++11/hidden/main.cpp
+++ src/c++11/hidden/main.cpp
@@ -10,6 +10,15 @@
 
 using namespace std;
 
+struct Foo {
+    string name;
+    int id;
+};
+
+void bar(Foo f) {
+    cout << f.name << " " << f.id << endl;
+}
+
 int main(int argc, const char * argv[]) {
     
     // function with __builtin_xxx are hidden. But not really! They are still
@@ -25,6 +34,36 @@ int main(int argc, const char * argv[]) {
     cout << "clz: " << clz << endl;
     cout << "popcount: " << popcount << endl;
     cout << "gcd: " << gcd << endl;
+    
+    
+    // odd expressions in c++ arrays
+    const char* name = "welcome to UC boulder!";
+    cout << name << endl;
+    
+    char x = "Hello" " " "World"[8];
+    cout << x << endl;
+    
+    char y = 8["Hello" " " "World"];
+    cout << y << endl;
+    
+    // crazy ternary operator
+    int a = 7;
+    int b = 8;
+    
+    ((a < b) ? a : b) = -((a > b) ? a : b);
+    
+    cout << a << " " << b << endl;
+    
+    
+    // You think you have mastered for (...) loop ?
+    // take a look at the below for loop & think again!
+    for (struct {int min; int max;} i = {0, 10}; i.min < i.max; i.min++) {
+        cout << i.min << endl;
+    }
+    
+    // I got you now, didn't I ? Haha.
+    bar(*(new Foo {"sunil", 45}));
+    // wait, I got into my own hell. How do I delete Foo now ??
     
     return 0;
 }
