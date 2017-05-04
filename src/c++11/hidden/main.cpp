cc0a9ae437328d69653cdb3359e877a1670a561b
Sunil <sunhick@gmail.com>
2017-05-03 21:55:34 -0600

    hidden c++ functions

---
+++ src/c++11/hidden/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  builtins
+//
+//  Created by Sunil on 5/3/17.
+//  Copyright © 2017 Sunil. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    // function with __builtin_xxx are hidden. But not really! They are still
+    // available. You can call them. Are they platform specific ? I don't know.
+    auto ffs = __builtin_ffs(10);
+    auto clz = __builtin_clz(10);
+    auto popcount = __builtin_popcount(10);
+    auto gcd = __gcd(10, 8);
+    
+    cout << "ffs: " << ffs << endl;
+    cout << "clz: " << clz << endl;
+    cout << "popcount: " << popcount << endl;
+    cout << "gcd: " << gcd << endl;
+    
+    return 0;
+}
