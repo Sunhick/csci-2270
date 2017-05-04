f3fe00b84cd98d3b0657cb17ed57f66c7b33685c
Sunil <sunhick@gmail.com>
2017-05-03 21:57:00 -0600

    list of builtin funcs

--- src/c++11/hidden/main.cpp
+++ src/c++11/hidden/main.cpp
@@ -14,6 +14,8 @@ int main(int argc, const char * argv[]) {
     
     // function with __builtin_xxx are hidden. But not really! They are still
     // available. You can call them. Are they platform specific ? I don't know.
+    
+    // List of builtin functions: http://www.keil.com/support/man/docs/armcc/armcc_chr1359125006834.htm
     auto ffs = __builtin_ffs(10);
     auto clz = __builtin_clz(10);
     auto popcount = __builtin_popcount(10);
