de21ef8c409d44ccc3ac640675bbbf85d34b4f09
Sunil <sunhick@gmail.com>
2017-11-30 21:42:05 -0800

    use of virtual destructor

---
+++ src/c++11/virtual-dtor/main.cpp
@@ -0,0 +1,64 @@
+/**
+ * main.cpp - Demonstrates the use of virtual destructor.
+ * Note: There's no virtual constructor.
+ *
+ * Copyright (C) 2017 by Sunil
+ */
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+namespace demo {
+
+class Foo {
+ public:
+  Foo();
+   virtual ~Foo();
+};  // class Foo
+
+// final keywords marks class can't be inherited.
+class Bar final : public Foo {
+ public:
+  Bar();
+   ~Bar();
+};  // class Bar
+
+// Error : Foobar can't extend final class.
+// class Foobar : Bar {
+//  public:
+//   Foobar();
+//   virtual ~Foobar();
+// }; // class Foobar
+
+
+Foo::Foo() {
+  cout << "Foo ctor" << endl;
+}
+
+Foo::~Foo() {
+  cout << "Foo dtor" << endl;
+}
+
+Bar::Bar() {
+  cout << "Bar ctor" << endl;
+}
+
+Bar::~Bar() {
+  cout << "Bar dtor" << endl;
+}
+
+}  // namespace demo
+
+int main(int argc, char *argv[argc]) {
+
+  using namespace demo;
+  
+  // comment out virtual dtor in Foo to see the what happens
+  // without virtual dtor, calling delete from base ptr will
+  // call derived type dtor.
+  Foo* f = new Bar;
+  delete f;
+  
+  return 0;
+}
